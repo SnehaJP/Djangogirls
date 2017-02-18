9252fcf740816e1e6b87b8b78b7a6575f98c1c3c
sneha <27sneha.jmp@gmail.com>
2017-02-18 14:31:47 +0100

    initial commit

---
+++ temp.py
@@ -0,0 +1,16 @@
+print('Hello, Django girls!')
+
+
+
+def greet():
+    print("please enter your name")
+    #name=input()
+    list=['sneha', 'emma', 'liliy']
+    for name in list:
+        print("hey "+name)
+        print("next name")
+
+
+
+
+greet()
